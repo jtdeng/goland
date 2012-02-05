99b9e3db6cf1f42347c767273e6b256b67c0a56c
James Deng <jtdeng@gmail.com>
2012-02-05 13:45:17 +0900

    minor change

--- src/cmd/PE.go
+++ src/cmd/PE.go
@@ -2,91 +2,101 @@ package main
 
 import (
 	"fmt"
-	"container/vector"
-	"http"
-	"http/pprof"
-	"regexp"
-	"net"
 	"log"
+	"net"
+	"net/http"
+	"net/http/pprof"
+	"regexp"
 	"runtime"
 	//"url"
 )
 
-
-
 type ProvisioningEngine struct {
-	Routers	vector.Vector
-	
+	Routers []router
 }
 
 //every resource should be able to handle the 4 operations
 type ResourceHanlder interface {
 	Create() //map to 'PUT'
-	Set()	//map to 'POST'
-	Get()	//map to 'GET'
-	Delete()	//map to 'DELETE'
+	Set()    //map to 'POST'
+	Get()    //map to 'GET'
+	Delete() //map to 'DELETE'
 }
 
 //url router, routes url to registered resource handlers
 type router struct {
 	p       string
-    cp      *regexp.Regexp
-    handler ResourceHanlder
+	cp      *regexp.Regexp
+	handler ResourceHanlder
 }
 
 //add a url router to routers list
 func (pe *ProvisioningEngine) AddRouter(pattern string, handler ResourceHanlder) {
 	cp, err := regexp.Compile(pattern)
 	if err != nil {
-        fmt.Printf("Invalid URL pattern: %q\n", pattern)
-        return
-    }
-    
-   pe.Routers.Push(router{pattern, cp, handler})
+		fmt.Printf("Invalid URL pattern: %q\n", pattern)
+		return
+	}
+
+	//pe.Routers.Push(router{pattern, cp, handler})
+	//pe.Routers[len(pe.Routers)] = router{pattern, cp, handler}
+    pe.Routers = append(pe.Routers, router{pattern, cp, handler})
 }
 
 func (pe *ProvisioningEngine) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-    //TODO: set content type according to resource format json or xml
-    resp.Header().Set("Content-Type", "application/json; charset=utf-8")
-    
-    //URL should be like /Feature/ResourceName/ResourceId&format=[xml|json]
-    //feature := req.URL.Path.Split()
-    fmt.Println(req.URL.Path)
-    
-    for i:=0; i< pe.Routers.Len(); i++ {
-        
-    }
+	//TODO: set content type according to resource format json or xml
+	resp.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	//URL should be like /Feature/ResourceName/ResourceId&format=[xml|json]
+	//feature := req.URL.Path.Split()
+	fmt.Println(req.URL.Path)
+
+	for i := 0; i < len(pe.Routers); i++ {
+        fmt.Println(pe.Routers[i].p)
+	}
+	
+	_handler :=  pe.GetHandler()
+	
+	switch _method := req.Method; _method {
+	case "GET": 
+        //call the Get() method of the handler object	
+	case "PUT":
+	
+	case "DELETE":
+	
+	case "POST":
+	
+	default:
+	   
+	}
+	
 }
 
 type Configuration map[string]string
 
-func (pe *ProvisioningEngine) Initialize(cfg Configuration){
+func (pe *ProvisioningEngine) Initialize(cfg Configuration) {
 	//read from config file and initialize PE
 }
 
+func (pe *ProvisioningEngine) Run(addr string) {
+	pe.Initialize(Configuration{"Name": "test"})
 
+	mux := http.NewServeMux()
 
-func (pe *ProvisioningEngine) Run (addr string) {
-	pe.Initialize(Configuration{"Name":"test"})
+	mux.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
+	mux.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
+	mux.Handle("/debug/pprof/heap", http.HandlerFunc(pprof.Heap))
+	mux.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
+	mux.Handle("/", pe) //calls pe.ServeHTTP
 
-    mux := http.NewServeMux()
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 
-    mux.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
-    mux.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
-    mux.Handle("/debug/pprof/heap", http.HandlerFunc(pprof.Heap))
-    mux.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
-    mux.Handle("/", pe) //calls pe.ServeHTTP
-
-
-    l, err := net.Listen("tcp", addr)
-    if err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }
-    
-    err = http.Serve(l, mux)
-    l.Close()
+	err = http.Serve(l, mux)
+	l.Close()
 }
-
 
 //////////////////////////////////////////////////////////////
 
@@ -105,9 +115,9 @@ func (hlr *HLRSubscriberHandler) Get() {
 func (hlr *HLRSubscriberHandler) Set() {
 }
 
-func init(){
-    runtime.GOMAXPROCS(4)
-    //TODO: load config from file
+func init() {
+	runtime.GOMAXPROCS(4)
+	//TODO: load config from file
 }
 
 func main() {
--- src/pkg/chat/server.go
+++ src/pkg/chat/server.go
@@ -9,108 +9,110 @@
 package chat
 
 import (
-    "log"
-    //"sync"
-    "netchan"
-    "strconv"
+	"log"
+	//"sync"
+	"netchan"
+	"strconv"
 )
 
 //the client representation is server side
 type client struct {
-	name		string
-	recvChan	chan []byte	//chann to recv msg from client
-	sendChan 	chan []byte //chan to send msg to client
+	name     string
+	recvChan chan []byte //chann to recv msg from client
+	sendChan chan []byte //chan to send msg to client
 }
 
 type Server struct {
-    //name        string
-    //broadcaster chan []byte
-    //receiver    chan []byte
-    tickets		int64
-    //ticketMutex	sync.Mutex	
-    exp         *netchan.Exporter
-    ticketReq	chan []byte
-    ticketRes	chan int64
-    clients		map[int64]*client
+	//name        string
+	//broadcaster chan []byte
+	//receiver    chan []byte
+	tickets int64
+	//ticketMutex	sync.Mutex	
+	exp       *netchan.Exporter
+	ticketReq chan []byte
+	ticketRes chan int64
+	clients   map[int64]*client
 }
 
 //handle ticket requests and assigning tickets to client
 //then export the channel base on this ticket
 func (s *Server) handleRequest() {
 	for {
-	    select { //no default, blocking
-	        case  req := <-s.ticketReq:
-	        	log.Println("Got ticket request from:", string(req))
-	            //assign a ticket
-	            s.tickets++
-	            //export channels, create a new client
-	            rChan, sChan := make(chan []byte), make(chan []byte)
-	            log.Println("Exporting channel:", "recv-"+strconv.Itoa64(s.tickets))
-	            s.exp.Export("recv-"+strconv.Itoa64(s.tickets), rChan, netchan.Recv)
-	            log.Println("Exporting channel:", "send-"+strconv.Itoa64(s.tickets))
-	            s.exp.Export("send-"+strconv.Itoa64(s.tickets), sChan, netchan.Send)
-	            s.clients[s.tickets] = &client{string(req), rChan, sChan}
-	    		//send ticket to client
-	    		s.ticketRes <- s.tickets
-	            s.exp.Drain(-1)
-	            log.Println("Sent ticket:", s.tickets)
-	            //ready to serve client
-	            log.Println("Serving channel:", s.tickets)
-	    		go s.handleSession(s.tickets)
-	    } 
-    }    
+		select { //no default, blocking
+		case req := <-s.ticketReq:
+			log.Println("Got ticket request from:", string(req))
+			//assign a ticket
+			s.tickets++
+			//export channels, create a new client
+			rChan, sChan := make(chan []byte), make(chan []byte)
+			log.Println("Exporting channel:", "recv-"+strconv.FormatInt(s.tickets, 10))
+			s.exp.Export("recv-"+strconv.FormatInt(s.tickets, 10), rChan, netchan.Recv)
+			log.Println("Exporting channel:", "send-"+strconv.FormatInt(s.tickets, 10))
+			s.exp.Export("send-"+strconv.FormatInt(s.tickets, 10), sChan, netchan.Send)
+			s.clients[s.tickets] = &client{string(req), rChan, sChan}
+
+			//send ticket to client
+			s.ticketRes <- s.tickets
+			s.exp.Drain(-1)
+			log.Println("Sent ticket:", s.tickets)
+
+			//ready to serve client
+			log.Println("Serving channel:", s.tickets)
+			go s.handleSession(s.tickets)
+		}
+	}
 }
 
-func (s *Server)handleSession(ticket int64) {
+func (s *Server) handleSession(ticket int64) {
 	for {
 		select {
-			case msg := <-s.clients[ticket].recvChan:
-				log.Println(s.clients[ticket].name, msg)
-				for k,v := range(s.clients) {
-					if k!=ticket {
-						log.Println("Forwarding message to", v.name)
-						//v.sendChan <- msg
-					}
+		case msg := <-s.clients[ticket].recvChan:
+			log.Println(s.clients[ticket].name, msg)
+			for k, v := range s.clients {
+				if k != ticket {
+					log.Println("Forwarding message to", v.name)
+					//v.sendChan <- msg
 				}
+			}
 		}
-	
-	}	
+
+	}
 }
 
-func (s *Server) Serve (addr string) {
-    //goroutine for tcp serve
-    //listener, err := net.Listen("tcp", addr)
-    //defer netlisten.Close()
-    //if err != nil {log.Fatal(err)}
-    //conn, err := listener.Accept()
-    //s.clients[conn.RemoteAddr().String()] = conn
-    //go s.exp.ServeConn(conn)
-    
-    go s.exp.ListenAndServe("tcp", addr)
-    
-    s.exp.Export("t-req", s.ticketReq, netchan.Recv)
-    s.exp.Export("t-res", s.ticketRes, netchan.Send)
-    s.handleRequest()
+func (s *Server) Serve(addr string) {
+	//goroutine for tcp serve
+	//listener, err := net.Listen("tcp", addr)
+	//defer netlisten.Close()
+	//if err != nil {log.Fatal(err)}
+	//conn, err := listener.Accept()
+	//s.clients[conn.RemoteAddr().String()] = conn
+	//go s.exp.ServeConn(conn)
+
+	go s.exp.ListenAndServe("tcp", addr)
+
+	s.exp.Export("t-req", s.ticketReq, netchan.Recv)
+	s.exp.Export("t-res", s.ticketRes, netchan.Send)
+	s.handleRequest()
 
 }
 
-func NewServer() (*Server) {
-    
-    exp := netchan.NewExporter()
-    
-    //in := make(chan []byte)
-    //err := exp.Export("broadcaster", in, netchan.Send)
-    //if err != nil { log.Fatal(err) }
-        
-    //out := make(chan []byte)
-    //err = exp.Export("receiver", out, netchan.Recv)
-    //if err != nil { log.Fatal(err) }
-    reqChan := make(chan []byte)
-    resChan := make(chan int64)
-    s := &Server{	tickets:0, 
-    				exp:exp, 
-    				ticketReq:reqChan, 
-    				ticketRes:resChan,
-    				clients: make(map[int64]*client)}
-    return s
+func NewServer() *Server {
+
+	exp := netchan.NewExporter()
+
+	//in := make(chan []byte)
+	//err := exp.Export("broadcaster", in, netchan.Send)
+	//if err != nil { log.Fatal(err) }
+
+	//out := make(chan []byte)
+	//err = exp.Export("receiver", out, netchan.Recv)
+	//if err != nil { log.Fatal(err) }
+	reqChan := make(chan []byte)
+	resChan := make(chan int64)
+	s := &Server{tickets: 0,
+		exp:       exp,
+		ticketReq: reqChan,
+		ticketRes: resChan,
+		clients:   make(map[int64]*client)}
+	return s
 }
