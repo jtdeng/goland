8866097d059959411eb1b4f2e3c6785a93517807
James Deng <jtdeng@gmail.com>
2012-02-06 23:23:07 +0900

    added test data first

---
+++ src/JSONQuery/doc.go
@@ -0,0 +1,13 @@
+// Copyright 2012 James Deng. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+  Package template implements JSONPath specification defined 
+  by Stefan Groessner at http://goessner.net/articles/JsonPath/
+
+  detailed specification goes here
+  ...
+*/
+
+package JSONQuery
